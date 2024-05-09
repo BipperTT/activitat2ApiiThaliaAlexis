ac265dc2a9d0dd3f5a3d4aaa7c18f2a7b7e455f0
Alexis <153605976+BipperTT@users.noreply.github.com>
2024-05-09 23:53:37 +0000

    actualizar login.proc.php

--- backend/usuario/login.proc.php
+++ backend/usuario/login.proc.php
@@ -2,36 +2,28 @@
 session_start();
 
 if (isset($_POST['email'], $_POST['password'])) {
+
     $correoUsuario = $_POST['email'];
     $contrasenaUsuario = $_POST['password'];
 
-    $url = "api/USUARI.php";
-    $data = array(
-        'usu_nom' => $correoUsuario,
-        'usu_contra' => $contrasenaUsuario
-    );
+    // establecer la conexión con la base de datos
+    $pdo = new PDO('mysql:host=localhost;dbname=fct', 'david', 'qwe123QWE123*');
 
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
-    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    $response = curl_exec($ch);
+    $stmt = $pdo->prepare("SELECT usu_nivell FROM USUARI WHERE usu_mail = :correoUsuario");
+    $stmt->execute(array('correoUsuario' => $correoUsuario));
+    $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    if (curl_errno($ch)) {
-        echo 'Error: ' . curl_error($ch);
-    } else {
-        $json_response = json_decode($response, true);
+    if ($usuario) {
+        if (password_verify($contrasenaUsuario, $usuario['usu_contra'])) {
 
-        if (isset($json_response['token'])) {
-            $_SESSION['token'] = $json_response['token'];
-            $nivelUsuario = $json_response['nivel'];
+            $_SESSION['token'] = generarToken();
+            $nivelUsuario = $usuario['usu_nivell'];
 
             if ($nivelUsuario === 'admin') {
-                header('Location: ADMIN.php');
+                header('Location: ../../api/ADMIN.php');
                 exit();
             } elseif ($nivelUsuario === 'user') {
-                header('Location: USER.php');
+                header('Location: ../../api/USER.php');
                 exit();
             } else {
                 echo "Error: Nivel de usuario desconocido";
@@ -41,8 +33,12 @@ if (isset($_POST['email'], $_POST['password'])) {
             header('Location: ../login.php?error=1');
             exit();
         }
+    } else {
+        header('Location: ../login.php?error=2');
+        exit();
     }
 } else {
+
     echo "Error: Datos de formulario no enviados";
     exit();
 }
